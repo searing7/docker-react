2475f9efad5a75d03aff81a255d014026a07f761
Abigail Resek <abigailresek@Abigails-MBP.attlocal.net>
2019-05-27 08:54:05 -0500

    changed txt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I R Learning
+          I was changed on feature branch brodaddy
         </p>
         <a
           className="App-link"
